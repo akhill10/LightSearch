44bc5d03d668b49eeb968c88c5f1c4e989756a37
Pawan Akhil <pawan.dasari@mutualmobile.com>
2021-05-30 17:18:43 +0200

    Commit message

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let test: string ;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
